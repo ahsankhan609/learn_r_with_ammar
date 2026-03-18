c73bf9a61f50ec1e54db75e50db7e593aeb751a8
ahsankhan609 <ahsankhan609@gmail.com>
2026-03-18 21:04:37 +0500

    initial commit

---
+++ 1_arithmetic_operations.R
@@ -0,0 +1,25 @@
+### Basic Commenting & Arithmetic Operations----
+# i am a comment
+
+2+3 #we add 2 in 3
+
+x = 2
+y = 3
+a = 5
+
+z = x + y +a
+z
+x*y*a
+a/x
+a^y
+
+### Some other Operators----
+#in R we use <- as equivalent to =
+a<- 5
+b<- 5
+c<- 5
+
+a+b+c
+a*b*c
+a^b
+print(a+b)
